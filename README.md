574d47d2140a1b6924e8e5fad5785ae8e0799d20
Markku Sukanen <markku.sukanen@gmail.com>
2025-11-09 08:59:33 +0200

    extra comments. Impl `IsOne` for all primitive integer types.

--- README.md
+++ README.md
@@ -2,21 +2,22 @@
 
 This library contains various "dice rolling" functions and traits.
 
-# dicebag::DiceExt
+## dicebag::DiceExt
 
 Dice rolling is currently implemented for most basic numeric types from i8 to i128, u8 to u128, and usize.
 
 ## Usage
 
-```
+```rust
+use dicebag::DiceExt;
 // Some normie dice:
 let a = 3.d6();   // 3d6
 let b = 10.d10(); // 10d10
 
 // Delta variancy:
 let d = 1.0.delta(5);
-      → 1.0 ± 5%
-      → 0.95 .. 1.05
+    //→ 1.0 ± 5%
+    //→ 0.95 .. 1.05
 
 // 50% chance of `c` becoming 5 (otherwise 0).
 let c = chance_of!(5, 50);
--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,53 @@
 //! In most cases, anything from `i8`/`u8` up to `i128`/`u128`
 //! and `usize` is supported (alongside `f32`/`f64` for a few
 //! functions).
-
+//! 
+//! # The Extensions
+//! 
+//! ## `DiceExt`
+//! 
+//! Covers the core intent dice rollings, e.g. `3.d6()`, `2.d10()`.
+//! ```
+//! use dicebag::DiceExt;
+//! let a = 3.d6();
+//! let b = 2_u8.d4();
+//! ```
+//! 
+//! ## `HiLo`
+//! 
+//! Coin flipping, using whatever datatype you implement it for…
+//! Comes with two convenience macros so that you don't need to
+//! write those yourself:
+//! ```
+//! use dicebag::{DiceExt, HiLo, lo, hi};
+//! if lo!() {/* do something if result was "low" */}
+//! if hi!() {/* do something if result was "high" */}
+//! ```
+//! 
+//! ## `InclusiveRandomRange`
+//! 
+//! To get a random value within given range.
+//! ```
+//! use dicebag::InclusiveRandomRange;
+//! let range = 6..=12;
+//! let v = range.random_of();
+//! ```
+//! 
+//! ## `RandomOf<T>`
+//! 
+//! A trait to get some random entry of e.g. [Vec].
+//! ```
+//! use dicebag::RandomOf;
+//! let v = vec![2,4,6,8,10];
+//! let x: i32 = v.random_of();
+//! 
+//! #[derive(Clone)]
+//! struct Abc { tag: String };
+//! let abc = vec![Abc{tag:"a".into()}, Abc{tag:"b".into()}, Abc{tag:"c".into()}];
+//! let x = abc.random_of();
+//! assert!(x.tag == "a" || x.tag == "b" || x.tag == "c");
+//! ```
+//! 
 use rand::Rng;
 use num::{ Float, Integer, NumCast, ToPrimitive };
 use paste::paste;
@@ -11,6 +57,18 @@ use paste::paste;
 pub type DiceT = (i32,i32);
 
 /// Dice extensions.
+/// 
+/// Note that there is no safeguard against overflows — it's up to you
+/// to ensure your dice rolls will fit into the datatype you're using.
+///
+/// # Example Usage
+/// ```
+/// use dicebag::DiceExt;
+/// let roll = 3.d6();    // i32 in, i32 out
+/// let roll = 2_u8.d8(); // u8 in, u8 out
+/// // hypothetical 15-sided die:
+/// let roll = 5.d(15);
+/// ```
 pub trait DiceExt {
     /// Roll any D.
     fn d(&self, sides: usize) -> Self;
@@ -36,6 +94,7 @@ pub trait DiceExt {
     fn d100(&self) -> Self;
 }
 
+/// Couple "coin-flip" extensions…
 pub trait HiLo {
     /// Value is considered "high"?
     fn hi(&self) -> bool;
@@ -55,21 +114,42 @@ pub trait FixedNumberVariance<T: Float> {
     fn upto_delta(&self, upto: T) -> T;
 }
 
+/// "It's just one, isn't it?"…
 pub trait IsOne {
+    /// A convenience extension…
+    /// 
+    /// `if something.is_one() {..}` vs `if something == 1 {..}`.
     fn is_one(&self) -> bool;
 }
 
-impl IsOne for i32 {
-    fn is_one(&self) -> bool {
-        *self == 1
-    }
+macro_rules! implement_isone_prim {
+    ($prim:expr) => {paste!{
+        impl IsOne for [<i $prim>] { fn is_one(&self) -> bool {*self == 1 }}
+        impl IsOne for &[<i $prim>] { fn is_one(&self) -> bool {**self == 1 }}
+        impl IsOne for [<u $prim>] { fn is_one(&self) -> bool {*self == 1 }}
+        impl IsOne for &[<u $prim>] { fn is_one(&self) -> bool {**self == 1 }}
+    }};
 }
+// Implement `IsOne` for all primitive integer types.
+implement_isone_prim!(8);
+implement_isone_prim!(16);
+implement_isone_prim!(32);
+implement_isone_prim!(64);
+implement_isone_prim!(128);
+implement_isone_prim!(size);
 
 pub trait InclusiveRandomRange {
     fn random_of(&self) -> i32;
 }
 
 impl InclusiveRandomRange for std::ops::RangeInclusive<i32> {
+    /// Generate random value within the given range.
+    /// 
+    /// ```
+    /// use dicebag::InclusiveRandomRange;
+    /// let range = 6..=12;
+    /// let roll = range.random_of();
+    /// ```
     fn random_of(&self) -> i32 {
         rand::rng().random_range(*self.start()..=*self.end())
     }
@@ -84,6 +164,7 @@ impl<T> RandomOf<T> for Vec<T>
 where T: Clone
 {
     type Output = T;
+    /// Get a random item from some vector.
     fn random_of(&self) -> Self::Output {
         T::clone(&self[1.d(self.len())-1]).clone()
     }
@@ -138,6 +219,7 @@ implement_sign_dependant_diceext!(i16, signed);
 implement_sign_dependant_diceext!(i32, signed);
 implement_sign_dependant_diceext!(i64, signed);
 implement_sign_dependant_diceext!(i128, signed);
+implement_sign_dependant_diceext!(isize, signed);
 implement_sign_dependant_diceext!(u8, unsigned);
 implement_sign_dependant_diceext!(u16, unsigned);
 implement_sign_dependant_diceext!(u32, unsigned);
